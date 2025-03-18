1f7c55a81e691215237dd4b2dc35d4be40e3f5bf
Georgs1xth <zhorik06bondarenko@gmail.com>
2025-03-18 11:06:18 +0500

    initialized routers

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-const PORT = process.env.PORT
+const models = require('./models/models')
+const PORT = process.env.PORT || 8008
+const cors = require('cors')
+const router = require('./routes/index')
 
-const app = express()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/api', router);
+
+app.get('/', (_req, res) => {
+    res.status(200).json({message: "Working!"})
+})
 
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT} \n LINK: http://localhost:${PORT}`))
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on port ${PORT} \n LINK: http://localhost:${PORT}`));
         
     } catch(e) {
-        console.log(e)
+        console.log(e);
     }
 }
 
-start()
+start();
