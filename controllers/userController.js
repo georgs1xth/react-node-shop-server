c0c10983abc81a1712a7c3137c39d801821e9424
Georgs1xth <zhorik06bondarenko@gmail.com>
2025-03-18 14:47:03 +0500

    58:51

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,23 @@
-const ApiError = require('../error/ApiError')
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const {User, Basket} = require('../models/models')
+
 
 class UserController {
     async registration(req, res) {
-
+        const {email, password, role} = req.body;
+        if(!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или password'))
+        }
+        const candidate = await User.findOne({
+            where: {email}
+        })
+        if(candidate){
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        
     }
     
     async login(req, res) {
--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const sequelize = require('./db');
 const models = require('./models/models');
 const PORT = process.env.PORT || 8008;
 const cors = require('cors');
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index');
 const errorHandlingMiddleware = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use('/api', router);
 
 app.use(errorHandlingMiddleware);
